67e149979c36cd80a52c2fab4a43452a5308ab08
sunwhite <sunwhite@126.com>
2020-02-24 18:54:33 +0800

    add master.c

---
+++ srs_types.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) sunwhite
+ */
+
+#ifndef _SRS_TYPES_H_INCLUDED_
+#define _SRS_TYPES_H_INCLUDED_
+
+#include <srs_config.h>
+
+#ifndef _int8_t_defined
+#define _int8_t_defined
+    typedef int8_t srs_int8_t;
+#endif
+#ifndef _int16_t_defined
+#define _int16_t_defined
+    typedef int16_t srs_int16_t;
+#endif
+#ifndef _int32_t_defined
+#define _int32_t_defined
+    typedef int32_t srs_int32_t;
+#endif
+#ifndef _int64_t_defined
+#define _int64_t_defined
+    typedef int64_t srs_int64_t;
+#endif
+
+#ifndef _uint8_t_defined
+#define _uint8_t_defined
+    typedef uint8_t srs_uint8_t;
+#endif
+#ifndef _uint16_t_defined
+#define _uint16_t_defined
+    typedef uint16_t srs_uint16_t;
+#endif
+#ifndef _uint32_t_defined
+#define _uint32_t_defined
+    typedef uint32_t srs_uint32_t;
+#endif
+#ifndef _uint64_t_defined
+#define _uint64_t_defined
+    typedef uint64_t srs_uint64_t;
+#endif
+
+#endif /* _SRS_TYPES_H_INCLUDED_ */
+
